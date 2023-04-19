92430ea0edd6ee150fef060d276a8cc3ea62d5a4
Khang Vo (doublevkay) <vovikhangcdv@gmail.com>
2023-04-19 14:57:19 +0700

    fix: add multi-platform configs for slsa-goreleaser

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # slsa-demo-go
 
 SLSA applying demo for Go - Guess Number Game
+
+## Setup
+
+Setup `slsa-goreleaser` configs for multi-platform builds
+
+    ```bash
+    python3 scripts/setup.py
+    ```
+
+## Release
+
+Trigger a release then trigger slsa-goreleaser to build and upload the release artifacts.
---
+++ scripts/generate-slsa-goreleaser-config.py
@@ -0,0 +1,56 @@
+import os
+
+matrix = {
+    "os": ["linux", "windows", "darwin"],
+    "arch": ["amd64", "arm64"]
+}
+
+template = """# Filename: ${{matrix.os}}-${{matrix.arch}}.yml
+# Version for this file.
+version: 1
+
+# (Optional) List of env variables used during compilation.
+env:
+    - GO111MODULE=on
+    - CGO_ENABLED=0
+
+# (Optional) Flags for the compiler.
+flags:
+    - -trimpath
+    - -tags=netgo
+
+# The OS to compile for. `GOOS` env variable will be set to this value.
+goos: {os}
+
+# The architecture to compile for. `GOARCH` env variable will be set to this value.
+goarch: {arch}
+
+# (Optional) Entrypoint to compile.
+# main: ./path/to/main.go
+
+# (Optional) Working directory. (default: root of the project)
+# dir: ./relative/path/to/dir
+
+# Binary output name.
+# {{ .Os }} will be replaced by goos field in the config file.
+# {{ .Arch }} will be replaced by goarch field in the config file.
+binary: binary-guess-number-game-{{ .Os }}-{{ .Arch }}
+
+# (Optional) ldflags generated dynamically in the workflow, and set as the `evaluated-envs` input variables in the workflow.
+ldflags:
+    - "-X main.Version={{ .Env.VERSION }}"
+    - "-X main.Commit={{ .Env.COMMIT }}"
+    - "-X main.CommitDate={{ .Env.COMMIT_DATE }}"
+    - "-X main.TreeState={{ .Env.TREE_STATE }}" """
+
+output_directory = ".github/slsa-goreleaser-configs"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+for os_value in matrix["os"]:
+    for arch_value in matrix["arch"]:
+        config_content = template.format(os=os_value, arch=arch_value)
+        filename = f"{output_directory}/{os_value}-{arch_value}.yml"
+        with open(filename, "w") as f:
+            f.write(config_content)
