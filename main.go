853674a0bf41d8a9597414c94b57b921c73ee718
Khang Vo (doublevkay) <vovikhangcdv@gmail.com>
2023-04-19 15:59:11 +0700

    feat: add progress bar for the game, add go.mod and go.sum

--- main.go
+++ main.go
@@ -1,22 +1,35 @@
-// Write a simplest game binary for go
-
 package main
 
 import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	var (
-		secretNumber = rand.Intn(100)
+		secretNumber = rand.Intn(100) + 1
 		guess        int
+		maxGuesses   = 10
+		guessCount   = 0
 	)
+
+	bar := progressbar.Default(int64(maxGuesses), "Guesses remaining")
+
 	for {
 		fmt.Println("Guess a number between 1 and 100")
 		fmt.Scan(&guess)
+		guessCount++
+		_ = bar.Add(1)
+
+		if guessCount == maxGuesses {
+			fmt.Printf("You've reached the maximum number of guesses. The secret number was %d. Better luck next time!\n", secretNumber)
+			break
+		}
+
 		switch {
 		case guess > secretNumber:
 			fmt.Println("Too high")
